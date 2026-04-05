05f257c9d91d766aeda09f25019c460532cc9a79
Yamuna <yamunayy111@gmail.com>
2026-04-05 23:40:25 +0530

    fix port issue

--- app.py
+++ app.py
@@ -59,5 +59,8 @@ def data():
 def download():
     return send_file("smart_crowd_results.csv", as_attachment=True)
 
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
